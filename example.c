429e3d5e050ed870a9be8ba34fab67bce5b7b062
Zack Sargent <zack@zack.fyi>
2024-11-19 16:21:32 -0500

    require more logic

--- example.c
+++ example.c
@@ -1,11 +1,29 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+bool allowed = false;
+bool counted = false;
+
+void allow() {
+	allowed = true;
+}
+
+void count() {
+	static count = 0;
+	count += 1;
+	if (count >= 3) {
+		counted = true;
+	}
+}
 
 void win() {
-	puts("you got the shell! here's the flag:");
-	char *args[] = {"cat", "flag.txt", NULL};
-	execve("/bin/cat", args, NULL);
+	if (allowed && counted) {
+		puts("you got the shell! here's the flag:");
+		char *args[] = {"cat", "flag.txt", NULL};
+		execve("/bin/cat", args, NULL);
+	}
 }
 
 void bof(char* input) {
