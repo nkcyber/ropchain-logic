5fdbee93b2a5d17c62b2f4155b1f0b85b0f09912
Zack Sargent <zack@zack.fyi>
2024-11-19 19:08:07 -0500

    fix message

--- example.c
+++ example.c
@@ -20,7 +20,7 @@ void count() {
 
 void win() {
 	if (allowed && counted) {
-		puts("you got the shell! here's the flag:");
+		puts("you won! here's the flag:");
 		char *args[] = {"cat", "flag.txt", NULL};
 		execve("/bin/cat", args, NULL);
 	}
